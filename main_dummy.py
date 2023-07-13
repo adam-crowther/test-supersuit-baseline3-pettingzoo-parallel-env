962dcbf8880655d7e80e286e38a4b7418f4242b0
Adam Crowther <github@adamcc.ch>
2023-07-13 15:30:13 +0200

    Added EvalCallback to demonstrate Issue 223

--- main_dummy.py
+++ main_dummy.py
@@ -1,6 +1,7 @@
 import supersuit as ss
 from pettingzoo.test import parallel_api_test
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import EvalCallback
 
 from dummy_env import dummy
 from dummy_env.sb3_shim_wrapper import Sb3ShimWrapper
@@ -12,8 +13,20 @@ if __name__ == '__main__':
     # env_parallel = ss.flatten_v0(env_parallel)
     env_parallel = ss.pettingzoo_env_to_vec_env_v1(env_parallel)
     env_parallel = ss.concat_vec_envs_v1(env_parallel, 1, base_class="stable_baselines3")
+
+    # This shim is required to work around Issue #222
+    # https://github.com/Farama-Foundation/SuperSuit/issues/222
     env_parallel = Sb3ShimWrapper(env_parallel)
 
     model = PPO("MlpPolicy", env_parallel, verbose=1)
-    
-    model.learn(total_timesteps=10_000)
+
+    # This EvalCallback is included to demonstrate Issue #223
+    # https://github.com/Farama-Foundation/SuperSuit/issues/223
+    eval_callback = EvalCallback(env_parallel,
+                                 best_model_save_path="./logs/",
+                                 log_path="./logs/",
+                                 eval_freq=50,
+                                 deterministic=True,
+                                 render=True,
+                                 verbose=1)
+    model.learn(total_timesteps=10_000, callback=eval_callback)
